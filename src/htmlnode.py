7463b1ce673a7e3536e90089babd5bfb1c16da74
ElphC350 <tk.correa89@gmail.com>
2024-12-26 14:18:56 -0600

    Ch2L1 TextNode and Ch2L4 LeafNode, with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,7 +9,33 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        return f"{self.PROPS.value}"
+        txt_rend = ""
+        if (self.PROPS == None or len(self.PROPS) == 0):
+            return txt_rend
+        for each in self.PROPS:
+            txt_rend = txt_rend + f' {each}="{self.PROPS[each]}"'
+        return txt_rend
+    
+    def __eq__(self, other):
+        return (self.TAG == other.TAG and self.VALUE == other.VALUE and self.CHILDREN == other.CHILDREN and self.PROPS == other.PROPS)
     
     def __repr__(self):
         return f"HTMLNode({self.TAG}, {self.VALUE}, {self.CHILDREN}, {self.PROPS})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+        
+        if hasattr(self, 'children') and len(self.CHILDREN) > 0:
+            raise ValueError("LeafNode instances do not allow for children")
+        
+        if self.VALUE == None:
+            raise ValueError("LeafNode instance requires [value] attribute that is a string. e.g: ''")
+        
+    def to_html(self):
+        if self.TAG == None:
+            return f"{self.VALUE}"
+        if self.PROPS == None:
+            return f"<{self.TAG}>{self.VALUE}</{self.TAG}>"
+        return f"<{self.TAG}{self.props_to_html()}>{self.VALUE}</{self.TAG}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,5 +2,59 @@ import unittest
 
 from htmlnode import *
 
+tag1 = "a"
+tag2 = "h1"
+val1 = "Here be some text to render"
+val2 = "Here is some text to render"
+chld1 = []
+chld2 = []
+prop1 = {
+    "href": "https://somesite.dev/",
+    "target": "_blank"
+}
+prop2 = {
+    "href": "https://www.somesite.dev/",
+    "target": "_blank"
+}
+
+
 class TestHTMLNode(unittest.TestCase):
-    pass
+    def test_heq1(self):
+        node1 = HTMLNode("a", "Here be some text to render")
+        node2 = HTMLNode("a", "Here be some text to render")
+        self.assertEqual(node1, node2)
+
+    def test_heq2(self):
+        node1 = HTMLNode(tag1, val1, None, None)
+        node2 = HTMLNode(tag1, val1, None, None)
+        chkProp1 = node1.props_to_html()
+        chkProp2 = node2.props_to_html()
+        self.assertEqual(chkProp1, chkProp2)
+
+    def test_heq3(self):
+        node3 = HTMLNode(tag1, val1, chld1, prop1)
+        node4 = HTMLNode(tag1, val1, chld1, prop1)
+        self.assertEqual(node3, node4)
+
+    def test_heq4(self):
+        node5 = HTMLNode(tag2, val2, chld2, prop2)
+        node6 = HTMLNode(tag2, val2, chld2, prop2)
+        self.assertEqual(node5, node6)
+
+    def test_Leq1(self):
+        Lnode1 = LeafNode(tag1, val1)
+        Lnode2 = LeafNode(tag1, val1)
+        self.assertEqual(Lnode1, Lnode2)
+
+    def test_Leq2(self):
+        Lnode3 = LeafNode(tag2, val2, prop2)
+        Lnode4 = LeafNode(tag2, val2, prop2)
+        self.assertEqual(Lnode3, Lnode4)
+
+    def test_Leq3(self):
+        Lnode5 = LeafNode(tag1, val2, prop2)
+        Lnode6 = LeafNode(tag1, val2, prop2)
+        self.assertEqual(Lnode5, Lnode6)
+
+if __name__ == "__main__":
+    unittest.main()
